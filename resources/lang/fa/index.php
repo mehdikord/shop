d313da23fe70ca2eeac48a5744cfc2a451c3b3a8
Mehdi kord <mehdi.kord171@gmail.com>
2021-08-30 00:22:38 +0430

    Start Project By Mehdi Kord

---
+++ resources/lang/fa/index.php
@@ -0,0 +1,15 @@
+<?php
+
+
+return[
+    
+    'site_name' => 'مهدی کرد | Mehdi Kord',
+    'menu_home' => 'صفحه اصلی',
+    'menu_contact' => 'تماس با من',
+    'menu_about' => 'درباره من',
+    
+
+
+];
+
+?>
